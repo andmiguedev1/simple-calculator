a5d75acb76e98bf22b4f79f4378e1b425f97c7c5
andresmgomez <andresmgomez23@gmail.com>
2023-07-02 22:42:32 -0500

    Refactor functions to use event listeners

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,75 +1,57 @@
-/**
- * Algorithm
- *
- * 1. Keep track of each number clicked by the user
- * 2. Convert to a list of numbers and store it in
- * a temporary variables as a string
- *
- * 3. Keep track of math operation clicked by user
- * 4. Repeat the first step and second step, for
- * the second selection
- *
- * 5. Check if math operation, matches what each button
- * holds as a value, then perform calculation on expression
- *
- * 6. Once user, clicked the clear button, set calculator
- * variables to its original values, or null
- */
-
 // Access calculator elements
+const numbers = document.querySelectorAll("[id*=num]");
+const operators = document.querySelectorAll("[id*=op]");
 const display = document.getElementById("display");
+const equal = document.getElementById("equal");
+const reset = document.getElementById("reset");
 
 // Initalize calculator variables
-let calculateSteps = 0;
-let pastNumber, mathOperator, nextNumber, result;
+let pastNumber, mathOperator;
+let newNumber = true;
 
-let firstNumbers = [];
-let secondNumbers = [];
-
-function getNumbers(number) {
-  if (calculateSteps === 0 || calculateSteps === 1) {
-    firstNumbers.push(number); // [7,5,4]
-    calculateSteps = 1;
-    pastNumber = Number(firstNumbers.join("")); // '754'
-    display.value = pastNumber;
-  } else if (calculateSteps === 2) {
-    secondNumbers.push(number);
-    nextNumber = Number(secondNumbers.join(""));
-    display.value = nextNumber;
+// If there is a new number, display it.
+// Otherwise put it next to the number
+// previously clicked on calculator
+const updateDisplay = (strNumber) => {
+  if (newNumber) {
+    display.textContent = strNumber;
+    newNumber = false;
+  } else {
+    display.textContent += strNumber;
   }
-}
+};
 
-function getMathOperator(operator) {
-  calculateSteps = 2;
-  mathOperator = operator;
-}
+const calculateResult = () => {
+  if (mathOperator !== undefined && !newNumber) {
+    const result = eval(
+      `${Number(pastNumber)}${mathOperator}${Number(display.textContent)}`
+    );
 
-function clearDisplay() {
-  display.value = 0;
-  pastNumber = null;
-  mathOperator = null;
-  nextNumber = null;
-  calculateSteps = 0;
-  firstNumbers = [];
-  secondNumbers = [];
-  result = 0;
-}
-
-function mathCalculator() {
-  if (mathOperator === "/") {
-    result = pastNumber / nextNumber;
-    display.value = result;
-  } else if (mathOperator === "*") {
-    result = pastNumber * nextNumber;
-    display.value = result;
-  } else if (mathOperator === "-") {
-    result = pastNumber - nextNumber;
-    display.value = result;
-  } else if (mathOperator === "+") {
-    result = pastNumber + nextNumber;
-    display.value = result;
-  } else if (mathOperator === "%") {
-    result = pastNumber % nextNumber;
-    display.value = result;
+    display.textContent = result;
   }
-}
+};
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    updateDisplay(e.target.textContent);
+  });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (e) => {
+    calculateResult();
+    pastNumber = display.textContent;
+    newNumber = true;
+    mathOperator = e.target.textContent;
+  });
+});
+
+equal.addEventListener("click", () => {
+  calculateResult();
+  newNumber = true;
+});
+
+reset.addEventListener("click", () => {
+  display.textContent = 0;
+  newNumber = true;
+});
