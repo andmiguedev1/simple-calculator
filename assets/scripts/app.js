c8e419d2fed36944c570d3643ecd62bde9780ca8
andresmgomez <andresmgomez23@gmail.com>
2023-07-02 20:32:30 -0500

    Calculate math expression and show result

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,75 @@
+/**
+ * Algorithm
+ *
+ * 1. Keep track of each number clicked by the user
+ * 2. Convert to a list of numbers and store it in
+ * a temporary variables as a string
+ *
+ * 3. Keep track of math operation clicked by user
+ * 4. Repeat the first step and second step, for
+ * the second selection
+ *
+ * 5. Check if math operation, matches what each button
+ * holds as a value, then perform calculation on expression
+ *
+ * 6. Once user, clicked the clear button, set calculator
+ * variables to its original values, or null
+ */
+
+// Access calculator elements
 const display = document.getElementById("display");
 
-let previousNumber, mathOperator, nextNumber;
-let calculateSteps = 0,
-  result = 0;
+// Initalize calculator variables
+let calculateSteps = 0;
+let pastNumber, mathOperator, nextNumber, result;
 
-function getNumber(number) {
-  display.value = number;
+let firstNumbers = [];
+let secondNumbers = [];
+
+function getNumbers(number) {
+  if (calculateSteps === 0 || calculateSteps === 1) {
+    firstNumbers.push(number); // [7,5,4]
+    calculateSteps = 1;
+    pastNumber = Number(firstNumbers.join("")); // '754'
+    display.value = pastNumber;
+  } else if (calculateSteps === 2) {
+    secondNumbers.push(number);
+    nextNumber = Number(secondNumbers.join(""));
+    display.value = nextNumber;
+  }
 }
 
 function getMathOperator(operator) {
-  display.value = operator;
+  calculateSteps = 2;
+  mathOperator = operator;
 }
 
 function clearDisplay() {
   display.value = 0;
+  pastNumber = null;
+  mathOperator = null;
+  nextNumber = null;
+  calculateSteps = 0;
+  firstNumbers = [];
+  secondNumbers = [];
+  result = 0;
 }
 
 function mathCalculator() {
-  console.log("Equal button was pressed");
+  if (mathOperator === "/") {
+    result = pastNumber / nextNumber;
+    display.value = result;
+  } else if (mathOperator === "*") {
+    result = pastNumber * nextNumber;
+    display.value = result;
+  } else if (mathOperator === "-") {
+    result = pastNumber - nextNumber;
+    display.value = result;
+  } else if (mathOperator === "+") {
+    result = pastNumber + nextNumber;
+    display.value = result;
+  } else if (mathOperator === "%") {
+    result = pastNumber % nextNumber;
+    display.value = result;
+  }
 }
